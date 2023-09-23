b93612061f23807c551d225bd2be6f3e4cace96d
amiribrahimkholif <amiribrahimkholif@gmail.com>
2023-09-23 21:54:59 +0300

    Add Category CRUD Operations

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,90 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _contex;
+
+        public CategoryController(ApplicationDbContext contex)
+        {
+            _contex = contex;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> categories = _contex.Categories.ToList();
+            return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category model)
+        {
+            if (!ModelState.IsValid) 
+                return View(model);
+          
+            _contex.Categories.Add(model);
+            _contex.SaveChanges();
+            TempData["success"] = "Category created successfully";
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var model = _contex.Categories.SingleOrDefault(c => c.Id == id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+            return View(model); ;
+        }
+        [HttpPost]
+        public IActionResult Edit(Category model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            _contex.Categories.Update(model);
+            _contex.SaveChanges();
+            TempData["success"] = "Category updated successfully";
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var model = _contex.Categories.SingleOrDefault(c => c.Id == id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+            return View(model); ;
+        }
+        [HttpPost , ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            var model = _contex.Categories.SingleOrDefault(c => c.Id == id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+            _contex.Categories.Remove(model);
+            _contex.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Categry Name")]
+        public string Name { get; set; }
+        [Range(0, 100 ,ErrorMessage = "Display Order must be between 0 and 100.")]
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+    }
+}
